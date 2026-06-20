412e2c06d612f67adf1c7d5533e2c0e0cfaf1af0
Yagya Vyas <yagnaakv99@gmail.com>
2026-06-20 23:56:30 +0530

    dragable functionality

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,50 @@ const socket = io();
 
 const chess = new Chess();
 
-const boardElement = document.querySelector("chessboard");
+const boardElement = document.querySelector(".chessboard");
 
 const draggedPiece = null;
 const sourceSquare = null;
 const playerRole = null;
 
+const renderBoard = () => {
+    const board = chess.board();
+    // boardElement.innerHTML=''
+    // console.log(board);
+
+    board.forEach((row, rowidx) => {
+        // console.log(row);
+        row.forEach((square, squareidx) => {
+            const squareElement = document.createElement("div");
+            squareElement.classList.add(
+                "square",
+                (row + square) % 2 === 0 ? "blackBlock" : "whiteBlock",
+            );
+            squareElement.dataset.row = rowidx;
+            squareElement.dataset.col = squareidx;
+
+            if (square) {
+                const pieceElement = document.createElement("div");
+
+                pieceElement.classList.add(
+                    "piece",
+                    square.color === "w" ? "white"
+                     : "black",
+                );
+
+                pieceElement.innerText=''
+                pieceElement.draggable=playerRole===square.color
+
+                pieceElement.addEventListener("dragstart",()=>{
+
+                })
+
+            }
+        });
+    });
+};
+renderBoard();
+
+const handleMove = () => {};
+
+const getUniquePiece = () => {};
